00ee45b7a1c18f74e5234a245bf8d12acad7b5f7
sushant10 <sushdude34@gmail.com>
2018-02-20 23:37:38 -0500

    Added base script

--- Remind_friend.php
+++ Remind_friend.php
@@ -1,3 +1,23 @@
 <?php 
 # remind friend to take her pills   
+// Get the PHP helper library from https://twilio.com/docs/libraries/php
+require_once "vendor/autoload.php";// Loads the library
+use Twilio\Rest\Client;
+
+// Your Account Sid and Auth Token from twilio.com/user/account
+$sid = getenv('TWILIO_ACCOUNT_SID');
+$token = getenv('TWILIO_AUTH_TOKEN');
+
+$client = new Client($sid, $token);
+
+$my_number = getenv('my_num');
+$her_number = getenv('her_num'); 
+
+$client->messages->create(
+    $her_number,
+    array(
+        'from' => $my_number,
+        'body' => "Wake up! Take your pills"
+    )
+);  
 ?>
