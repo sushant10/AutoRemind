580aca00ebe7fae188b7c7b5c19634fcfd4958d6
sushant10 <sushdude34@gmail.com>
2018-02-22 15:29:25 -0500

    Added Time relative array

--- Remind_friend.php
+++ Remind_friend.php
@@ -4,6 +4,19 @@
 require_once "vendor/autoload.php";// Loads the library
 use Twilio\Rest\Client;
 
+set_error_handler('exceptions_error_handler');
+
+function exceptions_error_handler($severity, $message, $filename, $lineno) {
+  if (error_reporting() == 0) {
+    return;
+  }
+  if (error_reporting() & $severity) {
+    throw new ErrorException($message, 0, $severity, $filename, $lineno);
+  }
+}
+
+
+date_default_timezone_set('America/New_York');//or change to whatever timezone you want
 // Your Account Sid and Auth Token from twilio.com/user/account
 $sid = getenv('TWILIO_ACCOUNT_SID');
 $token = getenv('TWILIO_AUTH_TOKEN');
@@ -11,15 +24,29 @@ $token = getenv('TWILIO_AUTH_TOKEN');
 $client = new Client($sid, $token);
 
 $my_number = getenv('my_num');
-$her_number = getenv('her_num'; 
+$her_number = getenv('her_num'); 
+$message = "";
 
 # TODO // add functionality to message different things at different times
 
+$messages = array(
+   08 => "Take pill X",
+   12 => "Take pill Y",
+   15 => "Take pill Z",
+);
+
+#trying out timely messages
+try{
+echo $messages[date("H")];
+}catch(Exception $e){
+	echo "Message not sent, time not correct";
+}
+/*
 $client->messages->create(
     $her_number,
     array(
         'from' => $my_number,
         'body' => "Wake up! Take your pills"
     )
-);  
+);*/  
 ?>
